0199ec63b138fd666da233d40c4ee67d0bdf598c
Segni <segnitsega6@gmail.com>
2024-10-03 23:19:07 -0700

    modified Weather.jsx and gave style in Weather.css

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png'
 
 function Weather() {
   return (
     <div className='weather'>
         <div className="search-bar">
             <input type="text" placeholder="Search"/>
-            <img src='' alt='' />
+            <img src= {search_icon} alt='' />
         </div>
     </div>
   )
