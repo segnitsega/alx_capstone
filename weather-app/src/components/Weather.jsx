f18f537322e367cfe912d4539344c113f24892c3
Segni <segnitsega6@gmail.com>
2024-10-04 02:41:28 -0700

    added functionality to the search bar and handled potential exceptions

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -11,6 +11,7 @@ import wind_icon from '../assets/wind.png'
 
 function Weather() {
 
+    const inputRef = useRef()
     const [weatherData, setWeatherData] = useState(false);
 
     const allIcons = {
@@ -31,12 +32,22 @@ function Weather() {
     }
 
     const search = async(city) => {
+        if( city === "" ){
+            alert("Enter City Name");
+            return;
+        }
         try {
             console.log(import.meta.env.VITE_APP_ID)
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
             const response = await fetch(url);
             const data = await response.json();
+
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
+
             console.log(data);
             const icon = allIcons[data.weather[0].icon] ||clear_icon;
             setWeatherData({
@@ -48,7 +59,8 @@ function Weather() {
             })
 
         } catch (error) {
-             console.error(error);
+            setWeatherData(false);
+            console.error("Error in fetching weather data");
         }
     }
     useEffect(()=>{
@@ -57,9 +69,13 @@ function Weather() {
   return (
     <div className='weather'>
         <div className="search-bar">
-            <input type="text" placeholder="Search"/>
-            <img src= {search_icon} alt='' />
+            <input ref={inputRef} type="text" placeholder="Search"/>
+            <img src= {search_icon} alt='' onClick={()=>search(inputRef.current.value)}/>
         </div>
+        {weatherData?<>
+        
+        
+
         <img src={weatherData.icon} alt="" className='weather-icon'/>
         <p className='temperature'>{weatherData.temperature}{"\u00B0"}c</p>
         <p className='location'>{weatherData.location}</p>
@@ -79,6 +95,8 @@ function Weather() {
                 </div>
             </div>
         </div>
+        </>:<></>}
+
     </div>
   )
 }
